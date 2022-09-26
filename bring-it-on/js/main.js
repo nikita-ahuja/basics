1664916a7b89ebb61be732f7fa6b11607f9b2d50
Nikita Ahuja <nahuja038@gmail.com>
2022-09-26 03:38:31 -0500

    init commit

---
+++ bring-it-on/js/main.js
@@ -0,0 +1,23 @@
+// *Variables*
+// Create a variable and console log the value
+  var num = 5
+  console.log(num);
+
+// Create a variable, add 10 to it, and alert the value
+var num2=10
+console.log(num2=10)
+
+// *Functions*
+// Create a function that subtracts 4 numbers and alerts the difference
+
+
+
+// Create a function that divides one number by another and returns the remainder
+
+// *Conditionals*
+// Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+
+// Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+
+//*Loops*
+//Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
---
+++ simple-functions/js/main.js
@@ -0,0 +1,46 @@
+//---Easy
+//create a function that subtracts two numbers and alerts the difference
+
+function addTwoNumbers(a,b) {
+  a+b
+  console.log(a-b);
+}
+
+addTwoNumbers(5,3);
+
+//create a function that divides three numbers and console logs the quotient
+
+function divideTwoNums(a,b) {
+  console.log(a/b);
+}
+
+//create a function that multiplys three numbers and returns the product
+
+function multiplyThree(a,b,c) {
+
+}
+
+
+//---Medium
+//create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+
+function someStuff(a,b,c) {
+  var sumOfFirstTwo = a+b
+  var remainder = sum%c
+}
+
+//---Hard
+//create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+
+function hard(a,b,c,d) {
+  var product = a*b
+  if (product > 100) {
+    product/10
+  }
+}
+
+
+// create function that has a loop that prints resolutions 365 times to console
+console.log(screen.width);
+
+document.querySelector('h1')
